98cd19be1e7918f4311bc7d01daee560ab0d1c71
KyunU-Lee <rbsdn3159@gmail.com>
2023-10-17 14:17:19 +0900

    1.1 exercise

--- Algorithm-Data_Structures/Algorithm-Data_Structures/ex1.1.cpp
+++ Algorithm-Data_Structures/Algorithm-Data_Structures/ex1.1.cpp
@@ -1,5 +1,27 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
 
 int main() {
+
+	int left = 20, right = 36, age;
+	cout << "What your age :";
+	cin >> age;
+
+	while (right - left > 1)
+	{
+		int mid = right - ((right - left) / 2);
+
+		string answer;
+
+		cout << "Age Bigger then " << mid << "? Answer Yes / No : ";
+		cin >> answer;
+
+		if (answer == "Yes") left = mid;
+		else right = mid;
+	}
+
+	cout << "Your age is " << right;
 
 }
