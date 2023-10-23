d2251e9869605466b5090e30f9acd8cb0771fa14
KyunU-Lee <rbsdn3159@gmail.com>
2023-10-23 10:14:02 +0900

    ex4.1

---
+++ Algorithm-Data_Structures/ch4.Recursive and Divide and Conquer Algorithm/code4-6.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int fibo(int N) {
+	cout << "fibo(" << N << ") 호출" << endl;
+
+	if (N == 0) return 0;
+	else if (N == 1) return 1;
+
+	int result = fibo(N - 1) + fibo(N - 2);
+	cout << N << " 번째 =" << result << endl;
+
+	return result;
+}
+
+int main() {
+	fibo(6);
+}
---
+++ Algorithm-Data_Structures/ch4.Recursive and Divide and Conquer Algorithm/ex4.1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> memo;
+
+int fibo(int N) {
+	if (N == 0) return 0;
+	else if (N == 1) return 0;
+	else if (N == 2) return 1;
+
+	if (memo[N] != -1) {
+		return memo[N];
+	}
+
+	return memo[N] = fibo(N - 1) + fibo(N - 2) + fibo(N - 3);
+}
+
+
+int main() {
+	int N;
+	cin >> N;
+
+	memo.assign(N, -1);
+
+	fibo(N);
+
+	for (int i = 0; i < (int)memo.size(); ++i) {
+		cout << "Fibo(" << i << ") : " << memo[i] << endl;
+	}
+
+
+
+}
